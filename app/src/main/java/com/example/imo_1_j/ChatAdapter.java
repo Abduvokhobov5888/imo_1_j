38f51419c37e4a90462c92c7f25a4f25f4effa6e
Abbosbek <vip.real.aa@gmail.com>
2022-07-28 14:26:59 +0500

    Imo 1

--- app/src/main/java/com/example/imo_1_j/ChatAdapter.java
+++ app/src/main/java/com/example/imo_1_j/ChatAdapter.java
@@ -1,11 +1,9 @@
 package com.example.imo_1_j;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
